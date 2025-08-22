17bdfc54018a59c09ea628668ca5cf593f3f2262
emreerdener <erdener.emre@gmail.com>
2025-08-22 15:02:09 -0500

    fix: ensure PrimaryActionsCard only renders after component is mounted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@
 import PrimaryActionsCard from "@/components/primaryActions/PrimaryActionsCard";
 import TransactionsTable from "@/components/transactions/TransactionsTable";
 import { useViewportSize } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const { width } = useViewportSize();
   const isMobile = width < 768;
 
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <>
-      {isMobile && <PrimaryActionsCard />}
+      {mounted && isMobile && <PrimaryActionsCard />}
 
       <TransactionsTable />
     </>
