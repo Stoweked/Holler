24334205a3643de75691a8b68b168a68e4173afd
emreerdener <erdener.emre@gmail.com>
2025-08-21 23:13:19 -0500

    Refactor ColorSchemeToggle component: wrap ActionIcon in Tooltip for improved accessibility and user experience

--- src/components/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle.tsx
@@ -4,6 +4,7 @@ import {
   ActionIcon,
   useMantineColorScheme,
   useComputedColorScheme,
+  Tooltip,
 } from "@mantine/core";
 import { Sun03Icon, Moon02Icon } from "hugeicons-react";
 
@@ -14,20 +15,27 @@ export function ColorSchemeToggle() {
   });
 
   return (
-    <ActionIcon
-      onClick={() =>
-        setColorScheme(computedColorScheme === "light" ? "dark" : "light")
-      }
-      variant="subtle"
-      size="lg"
-      radius="xl"
-      aria-label="Toggle color scheme"
+    <Tooltip
+      position="left"
+      label={`Switch to ${
+        computedColorScheme === "light" ? "dark" : "light"
+      } mode`}
     >
-      {computedColorScheme === "light" ? (
-        <Moon02Icon size={20} />
-      ) : (
-        <Sun03Icon size={20} />
-      )}
-    </ActionIcon>
+      <ActionIcon
+        onClick={() =>
+          setColorScheme(computedColorScheme === "light" ? "dark" : "light")
+        }
+        variant="subtle"
+        size="lg"
+        radius="xl"
+        aria-label="Toggle color scheme"
+      >
+        {computedColorScheme === "light" ? (
+          <Moon02Icon size={20} />
+        ) : (
+          <Sun03Icon size={20} />
+        )}
+      </ActionIcon>
+    </Tooltip>
   );
 }
