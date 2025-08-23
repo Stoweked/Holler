0ab0f4c9b8c382041fb5fbb5ea384bbc7304bf84
emreerdener <erdener.emre@gmail.com>
2025-08-23 13:11:32 -0500

    skeletons for transactions table

--- src/components/transactions/TransactionsTable.tsx
+++ src/components/transactions/TransactionsTable.tsx
@@ -1,26 +1,30 @@
-import { Skeleton, Stack, Table } from "@mantine/core";
-
-const elements = [
-  { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
-  { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
-  { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
-  { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
-  { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
-];
+import { Group, Skeleton, Stack } from "@mantine/core";
+import { useViewportSize } from "@mantine/hooks";
 
 export default function TransactionsTable() {
-  const rows = elements.map((element) => (
-    <Table.Tr key={element.name}>
-      <Table.Td>{element.position}</Table.Td>
-      <Table.Td>{element.name}</Table.Td>
-      <Table.Td>{element.symbol}</Table.Td>
-      <Table.Td>{element.mass}</Table.Td>
-    </Table.Tr>
-  ));
+  const { width } = useViewportSize();
+  const isMobile = width < 768;
 
   return (
-    <Stack p="md">
-      <Skeleton radius="lg" width="100%" height={800} />
+    <Stack p={isMobile ? "md" : "xl"}>
+      <Stack>
+        <Group wrap="nowrap">
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+          <Skeleton radius="xl" w="100%" maw={140} height={32} />
+        </Group>
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+        <Skeleton radius="lg" width="100%" height={80} />
+      </Stack>
     </Stack>
   );
 }
