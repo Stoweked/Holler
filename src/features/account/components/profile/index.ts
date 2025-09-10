4cb0adc7e8555d6c5bb8bad12c0b6f9d2a2496c1
emreerdener <erdener.emre@gmail.com>
2025-09-10 09:51:43 -0500

    Build fix

--- src/features/account/components/profile/index.ts
+++ src/features/account/components/profile/index.ts
@@ -1,5 +1,4 @@
 // src/features/settings/components/sections/account/profile/index.ts
-export * from "../../settings/components/AvatarUpload";
 export * from "./ProfileCard";
 export * from "./ProfileForm";
 export * from "./ProfileView";
